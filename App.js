7e7b865812b241a310d8d0ff88500eb0346bea49
Jonathan Payne <jonathan.b.payne@gmail.com>
2018-11-28 19:29:08 -0500

    more design

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { ScrollView } from 'react-native-gesture-handler';
 import { LinearGradient } from 'expo';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, Animated }
+import { StyleSheet, Text, View, TouchableHighlight, Animated, ART }
   from 'react-native';
+import * as d3 from 'd3';
+const { Surface, Group, Shape } = ART;
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class App extends React.Component {
@@ -29,149 +31,203 @@ export default class App extends React.Component {
       icon = this.icons['close'];
     }
     return (
-      <ScrollView>
-        <View style={styles.container}>
-          <Text style={styles.title}>MONEY ON MY MIND</Text>
+      <ScrollView style={styles.main}>
+        <View scrollEnabled={false} style={styles.container}>
+          <Text style={styles.title}>Money On My Mind</Text>
         </View>
-        <View style={styles.container}>
-          <Text style={styles.blockTitle}>Coming Up:</Text>
-          <View style={styles.row}>
-            <View style={styles.smallerBlock}>
-              <Text style={styles.date}> </Text>
-            </View>
-            <View style={styles.bigBlock}>
-              <Text style={styles.subTitle}>Meeting with Forrest</Text>
-              <Text style={styles.subText}>Wednesday 12/19/18 12:00pm</Text>
-              <Text style={styles.subText}>CVOEO</Text>
-            </View>
-            <View style={styles.smallBlock}>
-              <Text style={styles.circle}>2</Text>
-              <Text style={styles.days}>Days</Text>
-            </View>
-          </View>
-        </View>
-        <View
-          style={styles.container}>
-          <View style={styles.moreButton}>
+        <View style={styles.padding}>
+          <View style={styles.upcomingBox}>
+            <Text style={styles.blockTitle}>Coming Up:</Text>
             <View style={styles.row}>
-              <Text style={styles.moreButton}>MORE</Text>
-              <TouchableHighlight
-                style={styles.button}
-                onPress={this.toggle.bind(this)}
-                underlayColor="#fff">
-                <Icon
-                  style={styles.FAIcon}
-                  name={icon}
-                />
-              </TouchableHighlight>
-            </View>
-          </View>
-
-          {
-            this.state.expanded && (<View style={styles.row}>
-              {this.props.children}
               <View style={styles.smallerBlock}>
                 <Text style={styles.date}> </Text>
               </View>
               <View style={styles.bigBlock}>
                 <Text style={styles.subTitle}>Meeting with Forrest</Text>
-                <Text style={styles.subText}>Wednesday 12/26/18 2:00pm</Text>
+                <Text style={styles.subText}>Wednesday 12/19/18 12:00pm</Text>
                 <Text style={styles.subText}>CVOEO</Text>
               </View>
               <View style={styles.smallBlock}>
-                <Text style={styles.circle}>9</Text>
+                <Text style={styles.circle}>2</Text>
                 <Text style={styles.days}>Days</Text>
               </View>
-            </View>)
-          }
+            </View>
+            <View style={styles.moreButton}>
+              <View style={styles.row}>
+                <Text style={styles.moreButton}>MORE</Text>
+                <TouchableHighlight
+                  style={styles.button}
+                  onPress={this.toggle.bind(this)}
+                  underlayColor="#fff">
+                  <Icon
+                    style={styles.FAIcon}
+                    name={icon}
+                  />
+                </TouchableHighlight>
+              </View>
+            </View>
 
-        </View>
-        <View style={styles.container}>
-          <Text style={styles.blockTitle}>Progress:</Text>
-          <LinearGradient
-            colors={['#d4af37', '#ffdf00', '#cfb53b']}
-            style={{ alignItems: 'center', borderRadius: 90 }}>
-            <Text style={styles.bigCircle}>$150</Text>
-          </LinearGradient>
-          <Text style={styles.moreButton}> </Text>
-        </View>
-        <View style={styles.container}>
-          <Text style={styles.blockTitle}>Current Goals:</Text>
-          <View style={styles.row}>
-            <View style={styles.smallerBlock}>
-              <Text style={styles.date}> </Text>
-            </View>
-            <View style={styles.biggerBlock}>
-              <Text style={styles.subText}>Attend "Spend Smart 2/2" Workshop</Text>
-            </View>
+            {
+              this.state.expanded && (<View style={styles.row}>
+                {this.props.children}
+                <View style={styles.smallerBlock}>
+                  <Text style={styles.date}> </Text>
+                </View>
+                <View style={styles.bigBlock}>
+                  <Text style={styles.subTitle}>Meeting with Forrest</Text>
+                  <Text style={styles.subText}>Wednesday 12/26/18 2:00pm</Text>
+                  <Text style={styles.subText}>CVOEO</Text>
+                </View>
+                <View style={styles.smallBlock}>
+                  <Text style={styles.circle}>9</Text>
+                  <Text style={styles.days}>Days</Text>
+                </View>
+              </View>)
+            }
+
           </View>
-          <View style={styles.moreButton}>
+        </View>
+        <View style={styles.padding}>
+          <View style={styles.progressBox}>
+            <Text style={styles.blockTitle}>Progress:</Text>
+
+            {/* <Surface width={250} height={250}>
+            <Group x={250 / 2} y={250 / 2}>
+
+                {(d3.pie().value(d => d.price)(userPurchases)).map(section => ([
+  {
+    itemName: 'Mountain Dew',
+    price: 3
+  }])
+                  <Shape
+                    key={section.index}
+                    d={path(section)}
+                    stroke="#000"
+                    fill={`rgb(0,0,255)`}
+                    strokeWidth={1}
+                  />
+                ))
+              }
+            </Group>
+          </Surface> */}
+
+            <LinearGradient
+              colors={['#d4af37', '#ffdf00', '#cfb53b']}
+              style={{ alignItems: 'center', borderRadius: 90 }}>
+              <Text style={styles.bigCircle}>$150</Text>
+            </LinearGradient>
+            <Text style={styles.moreButton}> </Text>
+          </View>
+        </View>
+        <View style={styles.padding}>
+          <View style={styles.goalsBox}>
+            <Text style={styles.blockTitle}>Current Goals:</Text>
             <View style={styles.row}>
-              <Text style={styles.moreButton}>MORE</Text>
-              <TouchableHighlight
-                style={styles.button}
-                onPress={this.toggle.bind(this)}
-                underlayColor="#fff">
-                <Icon
-                  style={styles.FAIcon}
-                  name={icon}
-                />
-              </TouchableHighlight>
-            </View>
-          </View>
-
-          {
-            this.state.expanded && (<View style={styles.row}>
-              {this.props.children}
               <View style={styles.smallerBlock}>
                 <Text style={styles.date}> </Text>
               </View>
               <View style={styles.biggerBlock}>
-              <Text style={styles.subText}>Pay down past due bills</Text>
-              <Text style={styles.subText}> </Text>
-              <Text style={styles.subText}>Attend "Keys To Credit" workshop</Text>
-              <Text style={styles.subText}> </Text>
+                <Text style={styles.subText}>Attend "Spend Smart 2/2" Workshop</Text>
               </View>
-            </View>)
-          }
+            </View>
+            <View style={styles.moreButton}>
+              <View style={styles.row}>
+                <Text style={styles.moreButton}>MORE</Text>
+                <TouchableHighlight
+                  style={styles.button}
+                  onPress={this.toggle.bind(this)}
+                  underlayColor="#fff">
+                  <Icon
+                    style={styles.FAIcon}
+                    name={icon}
+                  />
+                </TouchableHighlight>
+              </View>
+            </View>
 
+            {
+              this.state.expanded && (<View style={styles.row}>
+                {this.props.children}
+                <View style={styles.smallerBlock}>
+                  <Text style={styles.date}> </Text>
+                </View>
+                <View style={styles.biggerBlock}>
+                  <Text style={styles.subText}>Pay down past due bills</Text>
+                  <Text style={styles.subText}> </Text>
+                  <Text style={styles.subText}>Attend "Keys To Credit" workshop</Text>
+                  <Text style={styles.subText}> </Text>
+                </View>
+              </View>)
+            }
+
+          </View>
         </View>
       </ScrollView>);
   }
 }
 
 const styles = StyleSheet.create({
+  main: {
+    backgroundColor: '#000',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'transparent',
     alignItems: 'center',
+    top: 0,
+  },
+  padding: {
+    padding: 10,
+    paddingBottom: 0,
+  },
+  upcomingBox: {
+    flex: 1,
+    backgroundColor: '#fff8c7',
+    alignItems: 'center',
+    borderRadius: 6,
+  },
+  progressBox: {
+    flex: 1,
+    backgroundColor: '#eee',
+    alignItems: 'center',
+    borderRadius: 6,
+  },
+  goalsBox: {
+    flex: 1,
+    backgroundColor: '#edf9f3',
+    alignItems: 'center',
+    borderRadius: 6,
   },
   row: {
     display: 'flex',
     flexDirection: 'row',
-    backgroundColor: '#fff',
+    backgroundColor: 'transparent',
     alignItems: 'center',
   },
   bigBlock: {
+    backgroundColor: 'transparent',
     flex: 5,
     alignItems: 'center',
     padding: 2,
   },
   biggerBlock: {
+    backgroundColor: 'transparent',
     flex: 7,
     alignItems: 'center',
     padding: 2,
   },
   smallBlock: {
+    backgroundColor: 'transparent',
     flex: 2,
     alignItems: 'center',
   },
   smallerBlock: {
+    backgroundColor: 'transparent',
     flex: 1,
     alignItems: 'center',
   },
   title: {
-    backgroundColor: '#585',
+    backgroundColor: '#689',
     color: '#fff',
     fontSize: 25,
     fontWeight: 'bold',
@@ -185,13 +241,13 @@ const styles = StyleSheet.create({
     color: '#558',
     fontSize: 16,
     fontWeight: 'bold',
-    paddingTop: 5,
+    paddingTop: 12,
     paddingLeft: 10,
     paddingBottom: 9,
     alignSelf: 'stretch',
     textAlign: 'left',
-    borderTopWidth: 1,
-    borderTopColor: '#9cc',
+    // borderTopWidth: 10,
+    // borderTopColor: '#000',
   },
   subTitle: {
     alignItems: 'center',
@@ -204,6 +260,7 @@ const styles = StyleSheet.create({
     textAlign: 'left',
   },
   subText: {
+    backgroundColor: 'transparent',
     alignItems: 'center',
     color: '#555',
     fontSize: 16,
@@ -223,7 +280,11 @@ const styles = StyleSheet.create({
     color: '#9cc',
     fontSize: 28,
   },
+  button: {
+    backgroundColor: 'transparent',
+  },
   moreButton: {
+    backgroundColor: 'transparent',
     alignItems: 'flex-end',
     color: '#9cc',
     fontSize: 16,
